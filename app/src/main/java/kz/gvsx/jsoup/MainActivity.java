88db7863de3f4f823dba9992f9663d9a476db87c
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2021-04-16 18:41:58 +0600

    Update MainActivity.java

--- app/src/main/java/kz/gvsx/jsoup/MainActivity.java
+++ app/src/main/java/kz/gvsx/jsoup/MainActivity.java
@@ -2,7 +2,6 @@ package kz.gvsx.jsoup;
 
 import android.os.Bundle;
 import android.text.Spanned;
-import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
